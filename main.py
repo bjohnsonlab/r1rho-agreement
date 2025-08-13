78bdba2367027c3dfad41755a4b8f4c9fa6f11d7
Simon Hulse <simonhulse@protonmail.com>
2025-08-13 14:06:27 -0400

    Clean up the MATLAB script. Incease the range of w1 values produced in the MATLAB.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from typing import ClassVar, Literal, Self
 import sys
 
+from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
@@ -24,8 +26,8 @@ class Data:
     def fetch(cls, temp: Literal[37, 57, 67, 77]) -> Self:
         matlab = loadmat(cls.MATLAB_DATA)
         tau = matlab["tau{}".format(temp)][0][0]
-        eric_nu1 = matlab["X"][0] / (2.0 * np.pi * 1.0e3)
-        eric_r1rho = matlab["R1rho_plot_{}_fixed".format(temp)][0]
+        eric_nu1 = matlab["w1"][0] / (2.0 * np.pi * 1.0e3)
+        eric_r1rho = matlab["curve{}".format(temp)][0]
         ringnmr_nu1 = []
         ringnmr_r1rho = []
         with open(cls.RINGNMR_PATH_TMPL.format(temp), "r") as fh:
@@ -43,13 +45,13 @@ class Data:
     @property
     def tau_str(self) -> str:
         if 1.0e-9 <= self.tau < 1.0e-6:
-            return "{:.1f}ns".format(1.0e9 * self.tau)
+            return "{:.1f} ns".format(1.0e9 * self.tau)
         elif 1.0e-6 <= self.tau < 1.0e-3:
-            return "{:.1f}μs".format(1.0e6 * self.tau)
+            return "{:.1f} μs".format(1.0e6 * self.tau)
         elif 1.0e-3 <= self.tau < 1.0:
-            return "{:.1f}ms".format(1.0e3 * self.tau)
+            return "{:.1f} ms".format(1.0e3 * self.tau)
         else:
-            return "{:.1f}s".format(self.tau)
+            return "{:.1f} s".format(self.tau)
 
 
 def get_data() -> dict[int, Data]:
@@ -61,8 +63,13 @@ def get_data() -> dict[int, Data]:
 
 def main():
     data = get_data()
-    fig, ax = plt.subplots()
-
+    fig, ax = plt.subplots(
+        gridspec_kw=dict(left=0.1, right=0.99, bottom=0.1, top=0.99)
+    )
+    legend_info: list[tuple[Line2D | Patch, str]] = [
+        (Line2D([0], [0], color="k", alpha=0.5), "Keeler, McDermott"),
+        (Line2D([0], [0], color="k", ls=":"), "RING NMR"),
+    ]
     xmin, xmax = float("inf"), float("-inf")
     for temp, datum in data.items():
         xmin = min(min(xmin, datum.eric_nu1[0]), datum.ringnmr_nu1[0])
@@ -70,21 +77,26 @@ def main():
         color = ax.plot(
             datum.eric_nu1,
             datum.eric_r1rho,
-            label="Eric {} ({}°C)".format(datum.tau_str, temp),
+            alpha=0.5,
         )[0].get_color()
         ax.plot(
             datum.ringnmr_nu1,
             datum.ringnmr_r1rho,
             color=color,
             ls=":",
-            label="RING {} ({}°C)".format(datum.tau_str, temp),
+        )
+        legend_info.append(
+            (
+                Patch(color=color),
+                "{}°C ($\\tau_{{\\text{{c}}}} =$ {})".format(temp, datum.tau_str)
+            )
         )
 
     ax.set_xlim(xmin, xmax)
     ax.set_xlabel("$\\nu_1$ (kHz)")
     ax.set_ylabel("$R_{1\\rho}$ (s$^{-1}$)")
     ax.set_yscale("log")
-    ax.legend()
+    ax.legend([x[0] for x in legend_info], [x[1] for x in legend_info])
     fig.savefig("r1rho.pdf")
 
 
